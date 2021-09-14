80620393f64c3e3d787f657757781e490ad2fbfd
Gustavo Galho <gustavo_galhos@dellteam.com>
2021-09-14 10:01:17 -0300

    teste de api utilizando sqlachemy

---
+++ api_atividades/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import column
+from sqlalchemy.sql.schema import ForeignKey
+
+engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         binds=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+class Pessoas(Base):
+    __tablename__ = "pessoas"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return "<Pessoa {}>".format(self.nome)
+
+
+class Atividades(Base):
+    __tablename__ = "atividades"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey("pessoas.id"))
+    pessoa = relationship("Pessoas")
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    init_db()
